0c0852bfdb80888bd72c89898ad446aafda15ce2
Marco Fiorito <marcofiorito1@gmail.com>
2022-08-24 18:51:51 -0300

    feat(project): add basic event boundary

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,27 +1,33 @@
 import * as React from 'react';
 
 import { TouchableOpacity, Text, StyleSheet, View } from 'react-native';
-import { ComponentTracking } from 'react-native-components-tracking';
+import { ComponentTracking, TouchEventBoundary, } from 'react-native-components-tracking';
 
 export default function App() {
   const baseOnPress = () => {
     console.warn('button was pressed')
   }
   return (
-    <View style={styles.container}>
-      <ComponentTracking
-        triggerFunctionKey={'onPress'}
-        trackEvent={(event:string) => {
-          console.warn('an event tracked',event)
-        }}
-        event={'BUTTON_CLICK'}
-        idKey={'HELLO_WORLD'}>
-        <TouchableOpacity
-          onPress={baseOnPress}>
-          <Text>Hello World</Text>
-        </TouchableOpacity>
-      </ComponentTracking>
-    </View>
+    <TouchEventBoundary trackEvent={(event:string) => {
+      console.warn('an event tracked with event boundary',event)
+    }}>
+      <View style={styles.container}>
+        <ComponentTracking
+          triggerFunctionKey={'onPress'}
+          trackEvent={(event:string) => {
+            console.warn('an event tracked',event)
+          }}
+          event={'BUTTON_CLICK'}
+          idKey={'HELLO_WORLD'}>
+          <TouchableOpacity
+            onPress={baseOnPress}
+            component-tracking-label={'hello_world'}
+          >
+            <Text>Hello World</Text>
+          </TouchableOpacity>
+        </ComponentTracking>
+      </View>
+    </TouchEventBoundary>
   );
 }
 
